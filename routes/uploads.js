99883a1e98de156fe5186ce324f8142cef3e997a
Abel Reynalte <abel.reynalte@tismart.com>
2021-02-28 23:58:55 -0500

    subir archivos

---
+++ routes/uploads.js
@@ -0,0 +1,32 @@
+const { Router } = require('express');
+const { check } = require('express-validator');
+
+const { validarCampos, validarArchivoSubir, validarJWT } = require('../middlewares');
+const { cargarArchivo, actualizarImagen, mostrarImagen, actualizarImagenCloudinary, mostrarImagenCloudinary } = require('../controllers/uploads');
+const { coleccionesPermitidas } = require('../helpers');
+
+
+const router = Router();
+
+
+router.post( '/', validarArchivoSubir, cargarArchivo );
+
+router.put('/:coleccion/:id', [
+    validarJWT,
+    validarArchivoSubir,
+    check('id','El id debe de ser de mongo').isMongoId(),
+    check('coleccion').custom( c => coleccionesPermitidas( c, ['usuarios','productos'] ) ),
+    validarCampos
+], actualizarImagenCloudinary )
+// ], actualizarImagen )
+
+router.get('/:coleccion/:id', [
+    check('id','El id debe de ser de mongo').isMongoId(),
+    check('coleccion').custom( c => coleccionesPermitidas( c, ['usuarios','productos'] ) ),
+    validarCampos
+], mostrarImagenCloudinary  )
+//], mostrarImagen  )
+
+
+
+module.exports = router;
